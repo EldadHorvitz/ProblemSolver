07abab41e54e92feb2c8fbd4dc4dfcbae6a583b5
NizanMandelblit <nizanm91@gmail.com>
2020-01-21 21:17:31 +0200

    MyTestClientHandler.cpp changes

--- MyTestClientHandler.cpp
+++ MyTestClientHandler.cpp
@@ -4,16 +4,53 @@
 
 #include <unistd.h>
 #include "MyTestClientHandler.h"
+#include <algorithm>
 
 using namespace std;
+
+
+
+void readFromBuffer(string buffer) {
+    std::string delimiter = ",";
+    size_t pos = 0;
+    std::string token[37];
+    int i = 0;
+    while ((pos = buffer.find(delimiter)) != std::string::npos) {
+        token[i] = buffer.substr(0, pos);
+        buffer.erase(0, pos + delimiter.length());
+        i++;
+        cout << token[i] << endl;
+    }
+    int c = 0;
+}
+
 
 void MyTestClientHandler::handleClient(int socket) {
     //reading from client
     char buffer[1024] = {0};
+    int numOfComma = 0;
+    string s = "";
+    int firstTime = 1;
     while (1) {
         read(socket, buffer, 1024);
-        cout << buffer;
-    }
+        string delimiter = "\n";
+        s += buffer;
+        size_t pos = 0;
+        string token;
+        while ((pos = s.find(delimiter)) != string::npos) {
+            token = s.substr(0, pos);
+            readFromBuffer(token);
+            s.erase(0, pos + delimiter.length());
+        }
+        if (firstTime) {
+            firstTime = 0;
+            numOfComma = std::count(token.begin(), token.end(), ',');
+        }
+        if (token == "end") {
+            break;
+        }
 
+    }
+int matrix[numOfComma][numOfComma];
 
 }
