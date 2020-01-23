d8cfc2e99b9c99c1e034881dec81e3c8cc6f041b
EldadHorvitz <eldad373@gmail.com>
2020-01-23 22:14:05 +0200

    AStar added

--- AStar.cpp
+++ AStar.cpp
@@ -53,10 +53,12 @@ bool has (queue <State<T>*> q,State<T> *s){
     }
     return false;
 }
+
 template<class T,class S>
 int AStar<T,S>::getNumLength(){
     return counter;
 }
+
 template<class T,class S>
 queue <State<T>*> priority(queue <State<T>*> q){
     queue <State<T>*> temp;
