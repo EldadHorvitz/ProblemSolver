a7da90b382e602e83b738968e9dfd403a8c822e1
NizanMandelblit <nizanm91@gmail.com>
2020-01-23 16:50:07 +0200

    added Problem.h and Point.h

--- Problem.cpp
+++ Problem.cpp
@@ -30,18 +30,21 @@ void Problem::setEnd(const Point &end) {
 void Problem::insertLine(string str) {
     std::string delimiter = ",";
     size_t pos = 0;
-    int i = 0;
+    vector<double> smallVector;
     while ((pos = str.find(delimiter)) != std::string::npos) {
         double d = (stod(str.substr(0, pos)));
+        smallVector.push_back(d);
         str.erase(0, pos + delimiter.length());
-        i++;
     }
+    double d = (stod(str.substr(0, pos)));
+    smallVector.push_back(d);
+    this->matrix.push_back(smallVector);
 }
 
 void Problem::insertStartPoint(string str) {
     std::string delimiter = ",";
     size_t pos = 0;
-    int p1 = (stoi(str.substr(0, pos)));
+    int p1 = (stod(str.substr(0, pos)));
     str.erase(0, pos + delimiter.length());
     int p2 = (stoi(str.substr(0, pos)));
     this->setStart(Point(p1, p2));
