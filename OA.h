ab10722921f6084ab27455bdb3c98d16b049524c
EldadHorvitz <eldad373@gmail.com>
2020-01-23 11:59:25 +0200

    none

---
+++ OA.h
@@ -0,0 +1,18 @@
+//
+// Created by eldad on 23/01/2020.
+//
+
+#ifndef EX4_OA_H
+#define EX4_OA_H
+
+
+#include "Solver.h"
+template <class P,class S>
+class OA: public Solver<P,S>{
+private:
+    //Searcher
+};
+
+
+
+#endif //EX4_OA_H
