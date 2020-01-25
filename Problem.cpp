e6c616af1ead7f9ecc03fe4ea862bf77c4e79af1
EldadHorvitz <eldad373@gmail.com>
2020-01-25 21:51:36 +0200

    template cpp file deleted

--- Problem.cpp
+++ Problem.cpp
@@ -37,7 +37,7 @@ void Problem::insertToState() {
     int j;
     for (i = 0; i < this->rowSize; i++) {
         for (j = 0; j < this->colSize; j++) {
-            Point *p = new Point(i, j);
+            Point p = Point(i, j);
             double value = this->matrix[i][j];
             State<Point> *o = new State<Point>(p, value);
             this->matrixStates.push_back(o);
