6eac33a11ac0479735efdcc2c7b99d225cb91cba
NizanMandelblit <nizanm91@gmail.com>
2020-01-26 13:37:54 +0200

    added Problem.h and Point.h

--- OA.h
+++ OA.h
@@ -15,31 +15,29 @@ using namespace std;
 #include "BstFS.h"
 #include "AStar.h"
 
-template <class P,class S>
-class OA: public Solver<P,S>{
+template<class P, class S>
+class OA : public Solver<P, S> {
 private:
-    Searcher<Point,string>* BFS1;
-    Searcher<Point,string>* DFS1;
-    Searcher<Point,string>* BstFS1;
-    Searcher<Point,string>* AStar1;
+    Searcher<Point, string> *BFS1;
+    Searcher<Point, string> *DFS1;
+    Searcher<Point, string> *BstFS1;
+    Searcher<Point, string> *AStar1;
 public:
     OA() {
-        this->BFS1=new BFS<Point,string>();
-        this->DFS1=new DFS<Point,string>();
-        this->BstFS1=new BstFS<Point,string>();
-        this->AStar1=new AStar<Point,string>();
+        this->BFS1 = new BFS<Point, string>();
+        this->DFS1 = new DFS<Point, string>();
+        this->BstFS1 = new BstFS<Point, string>();
+        this->AStar1 = new AStar<Point, string>();
     }
 
     S solve(P *p) {
         //string s1="BFS:\n"+this->BFS1->search(p)+"\n";
-          //string s2="DFS:\n"+this->DFS1->search(p)+"\n";
-        string s3="BstFS:\n"+this->BstFS1->search(p)+"\n";
-       // string s4="AStar:\n"+this->AStar1->search(p)+"\n";
-        return s3;
-
+        //string s2="DFS:\n"+this->DFS1->search(p)+"\n";
+        //string s3="BstFS:\n"+this->BstFS1->search(p)+"\n";
+        string s4 = "AStar:\n" + this->AStar1->search(p) + "\n";
+        return s4;
     }
 };
-
 
 
 #endif //EX4_OA_H
