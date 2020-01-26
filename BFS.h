13072a9943a7af0cd6f3fe89937422fc64824671
NizanMandelblit <nizanm91@gmail.com>
2020-01-26 16:41:30 +0200

    added Problem.h and Point.h

--- BFS.h
+++ BFS.h
@@ -9,36 +9,41 @@
 #include "Point.h"
 
 using namespace std;
+
 #include <queue>
 #include <list>
 #include <string>
-template<class T,class S>
-class BFS: public Searcher<T,S> {
+
+template<class T, class S>
+class BFS : public Searcher<T, S> {
 
 private:
     int counter;
 public:
 
-    BFS():counter(0) {}
+    BFS() : counter(0) {}
 
-    S search(Searchable<T>* s){
-        queue <State<T>*> open;
-        s->getInit()->setCostSum(1);
-        open.push(s->getInit());
-        s->getInit()->setVisited(true);
-        while (!open.empty()){
+    S search(Searchable<T> *s) {
+        State<T>* begin=s->getInit();
+        State<T> erd=State<T>((Point) begin->getState(),begin->getCost());
+
+        queue < State<T> * > open;
+        begin->setCostSum(1);
+        open.push(begin);
+        begin->setVisited(true);
+        while (!open.empty()) {
             counter++;
-            State<T> * n;
+            State<T> *n;
             n = open.front();
             open.pop();
             n->setVisited(true);
-            if (n->getState()==s->getGoal()->getState()){
-                return getSolution(n,s->getInit());
+            if (n->getState() == s->getGoal()->getState()) {
+                return getSolution(n, begin);
             }
-            list<State<T>*> l=s->getNeighbours(n);
-            for (State<T>* s1:l){
-                if (!s1->isVisited()){
-                    s1->setCostSum(1+n->getCostSum());
+            list<State<T> *> l = s->getNeighbours(n);
+            for (State<T> *s1:l) {
+                if (!s1->isVisited()) {
+                    s1->setCostSum(1 + n->getCostSum());
                     s1->setDad(n);
                     open.push(s1);
                 }
@@ -47,23 +52,28 @@ public:
         }
 
     }
-    int getNumLength(){
+
+    int getNumLength() {
         return counter;
     }
-    string getSolution(State<T> *goal,State<T> *origin) {
+
+    string getSolution(State<T> *goal, State<T> *origin) {
         string solution1 = "";
-        vector < State<T>  > v;
+        vector<State<T> > v;
         State<T> temp = *goal;
         int count = 0;
-        while (!(temp.getState() == origin->getState())) {
+        Point tempp=(Point) temp.getState();
+        Point originp=(Point)  origin->getState();
+        while ((tempp.getX() != originp.getX() || (tempp.getY() != originp.getY()))) {
             v.push_back(temp);
             temp = *(temp.getDad());
             count++;
+            tempp=(Point) temp.getState();
         }
         v.push_back(temp);
         int i;
-        State<T> cur=*goal;
-        State<T> son=*goal;
+        State<T> cur = *goal;
+        State<T> son = *goal;
         bool f = true;
         for (i = count; i > 0; --i) {
             cur = v[i];
@@ -73,8 +83,8 @@ public:
             } else {
                 f = false;
             }
-            Point pCur = (Point ) cur.getState();
-            Point pSon = (Point ) son.getState();
+            Point pCur = (Point) cur.getState();
+            Point pSon = (Point) son.getState();
             if (pCur.getX() > pSon.getX()) {
                 solution1 = solution1 + "Up (" + to_string(int(v[i - 1].getCostSum())) + ")";
             } else if (pCur.getX() < pSon.getX()) {
@@ -89,13 +99,7 @@ public:
     }
 
 
-
 };
-
-
-
-
-
 
 
 #endif //EX4_BFS_H
