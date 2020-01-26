e1e9d08aab8848cabe506fe880dbdc3826cf6864
EldadHorvitz <eldad373@gmail.com>
2020-01-26 19:05:07 +0200

    bfs&dfs updated

--- OA.h
+++ OA.h
@@ -18,19 +18,17 @@ using namespace std;
 template<class P, class S>
 class OA : public Solver<P, S> {
 private:
-    Searcher<Point, string> *BFS1;
-    Searcher<Point, string> *DFS1;
-    Searcher<Point, string> *BstFS1;
-    Searcher<Point, string> *AStar1;
+
 public:
     OA() {
-        this->BFS1 = new BFS<Point, string>();
-        this->DFS1 = new DFS<Point, string>();
-        this->BstFS1 = new BstFS<Point, string>();
-        this->AStar1 = new AStar<Point, string>();
+
     }
 
     S solve(P *p) {
+        Searcher<Point, string> *BFS1= new BFS<Point, string>();
+        Searcher<Point, string> *DFS1= new DFS<Point, string>();
+        Searcher<Point, string> *BstFS1= new BstFS<Point, string>();
+        Searcher<Point, string> *AStar1=new AStar<Point, string>();
        // string s1="BFS:\n"+this->BFS1->search(p)+"\n";
         string s2="DFS:\n"+this->DFS1->search(p)+"\n";
         //string s3="BstFS:\n"+this->BstFS1->search(p)+"\n";
